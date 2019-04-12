7b451720a5834922288059662b9b05a61ce1fc02
Roger <rdpender3@gmail.com>
2019-04-11 22:22:08 -0700

    added modal

---
+++ src/ApartmentModal.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import {
+  Animated,
+  Dimesions,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View
+} from "react-native";
+
+const { width, height } = Dimesions.get("window");
+
+export default class ApartmentModal extends Component {
+  state = {
+    position: new Animated.Value(this.props.isOpen ? 0 : height),
+    visible: this.props.isOpen
+  };
+
+  componentWillReceiveProps(nextPorps) {
+    if (!this.props.isOpen && nextProps.isOpen) {
+      this.animateOpen();
+    } else if (this.props.isOpen && !nextPorps.isOpen) {
+      this.animateClose();
+    }
+  }
+
+  animateOpen() {
+    this.setState({ visible: true }, () => {
+      Animated.timing(this.state.position, { toValue: 0 }).start();
+    });
+  }
+
+  animateClose() {
+    Animated.timing(this.state.position, { toValue: height }).start(() =>
+      this.setState({ visible: false })
+    );
+  }
+
+  render() {
+    if (!this.state.visible) {
+      return null;
+    }
+    return (
+      <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={this.props.onClose}>
+          <Animated.View style={styles.backdrop} />
+        </TouchableWithoutFeedback>
+        <Animated.View
+          style={[
+            styles.modal,
+            {
+              transform: [
+                { translateY: this.state.position },
+                { translateX: 0 }
+              ]
+            }
+          ]}
+        >
+          <Text>Modal</Text>
+        </Animated.View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    backgroundColor: "transparent"
+  },
+  backdrop: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "black",
+    opacity: 0.5
+  },
+  modal: {
+    height: height / 2,
+    backgroundColor: "white"
+  }
+});
